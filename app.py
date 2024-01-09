b5f31e15821cb6b0c9ac40f5ea0800c3670d5bf8
Collins Chetekei <chetekeicollins@gmail.com>
2024-01-09 05:59:15 +0300

    add:third_commit

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def home():
-    df = pd.read_csv('targets.csv')
-    names = df['TM']
-    output = names.to_list()
-    return output
+    # df = pd.read_csv('targets.csv')
+    # names = df['TM']
+    # output = names.to_list()
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
