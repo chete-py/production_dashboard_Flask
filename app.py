449c85e2c936d91c3cc79c28f0fd426d74b5b14a
Collins Chetekei <chetekeicollins@gmail.com>
2024-01-21 13:46:57 +0300

    add: corrected_url_mapping

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import gspread
 from google.oauth2 import service_account
-import openpyxl
 
 # Define your Google Sheets credentials JSON file (replace with your own)
 credentials_path = 'keys.json'
@@ -20,12 +19,12 @@ url_targ = "https://docs.google.com/spreadsheets/d/1yQXPZ4zdI8aiIzYXzzuAwDS1V_Zg
 worksheet_accounts = gc.open_by_url(url_acc).worksheet("accounts")
 worksheet_targets = gc.open_by_url(url_targ).worksheet("targets")
 
-data = worksheet_accounts.get_all_values()
+dataframe = worksheet_accounts.get_all_values()
     
 # Prepare data for Plotly
-headers = data[0]
-data = data[1:]
-newdf = pd.DataFrame(data, columns=headers)
+headers = dataframe[0]
+dataframe = dataframe[1:]
+newdf = pd.DataFrame(dataframe, columns=headers)
 five_rows = newdf.head(5)
 TARGETS = five_rows.to_dict(orient='records')
 
@@ -34,18 +33,21 @@ TARGETS = five_rows.to_dict(orient='records')
 df = None
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+
+app.secret_key = 'PfdRs1999@A-402'
 
 
 @app.route("/")
 def index(): 
     return render_template('form.html')
 
-@app.route("/home", methods=['GET', 'POST'])
-def home():
+@app.route("/upload_file" , methods=['POST'])
+def upload_file():
     global df
+
     if 'file' not in request.files:
         return 'No file part'
 
@@ -57,14 +59,36 @@ def home():
     if file:
         file_path = 'uploads/' + file.filename
         file.save(file_path)
+
+        # Store the file path in a session variable
+        session['uploaded_file_path'] = file_path
+
+        return redirect(url_for('home'))  # Redirect to the /home route for rendering home.html
+
+@app.route("/home")
+def home():
+    # Retrieve the uploaded file path from the session
+    file_path = session.get('uploaded_file_path', None)
+
+    if file_path:
         df = process_uploaded_file(file_path)
         return render_template('home.html', data=df, targets=TARGETS)
+    else:
+        return 'No uploaded file found.'
 
 
 @app.route("/tms")
-def tms():    
-    return render_template('tms.html', data=df, targets=TARGETS)
+def tms():
+    #Retrieve the uploaded file path from the session
+    file_path = session.get('uploaded_file_path', None)
 
+    if file_path:
+        df = process_uploaded_file(file_path)
+        return render_template('tms.html', data=df, targets=TARGETS)
+    else:
+        return 'No uploaded file found.'
+        
+    
 
 def process_uploaded_file(file_path):
     import pandas as pd
