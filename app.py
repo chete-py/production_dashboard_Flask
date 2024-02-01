def7470ba848a81b0fa9c84f5b8ffc08c1e8822a
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-01 22:08:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'chetekei007@gmail.com'
+app.config['MAIL_USERNAME'] = 'chetekeicollins@gmail.com'
 app.config['MAIL_PASSWORD'] = 'idtptgybyntrgunr'
 app.config['MAIL_DEBUG'] = True
 
